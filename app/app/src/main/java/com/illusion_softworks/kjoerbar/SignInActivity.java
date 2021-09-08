3f5ac571a3bceeb10627d123089987b54d279c69
JoakimAa <43811556+JoakimAa@users.noreply.github.com>
2021-09-08 20:45:35 +0200

    Flytta på noe greier
    Signed-off-by: JoakimAa <43811556+JoakimAa@users.noreply.github.com>

--- app/app/src/main/java/com/illusion_softworks/kjoerbar/SignInActivity.java
+++ app/app/src/main/java/com/illusion_softworks/kjoerbar/SignInActivity.java
@@ -2,7 +2,6 @@ package com.illusion_softworks.kjoerbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +11,10 @@ import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.firebase.ui.auth.util.ExtraConstants;
-import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,23 +33,49 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void createSignInIntent() {
-        // [START auth_fui_create_intent]
-        // Choose authentication providers
+        // ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+        //       .setAndroidPackageName(
+        //                /* yourPackageName= */ "com.illusion_softworks.kjoerbar",
+        //                /* installIfNotAvailable= */ true,
+        //                /* minimumVersion= */ null)
+        //     .setHandleCodeInApp(true) // This must be set to true
+        //    .setUrl("https://google.com") // This URL needs to be whitelisted
+        //   .build();
+
+
+
         List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder()
+                        //.enableEmailLinkSignIn()
+                        //.setActionCodeSettings(actionCodeSettings)
+                        .build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build());
 
-        // Create and launch sign-in intent
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setIsSmartLockEnabled(false)
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_create_intent]
-    }
+        /*
+        if (AuthUI.canHandleIntent(getIntent())) {
+            if (getIntent().getExtras() == null) {
+                return;
+            }
+            String link = getIntent().getExtras().getString(ExtraConstants.EMAIL_LINK_SIGN_IN);
+            if (link != null) {
+        */
+                // Create and launch sign-in intent
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        //.setEmailLink(link)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
+                        .setTosAndPrivacyPolicyUrls(
+                                "https://example.com/terms.html",
+                                "https://example.com/privacy.html")
+                        //.setLogo(R.drawable.my_great_logo)      // Set logo drawable
+                        //.setTheme(R.style.MySuperAppTheme)      // Set theme
+                        .build();
+                signInLauncher.launch(signInIntent);
+            }
+        //}
+    //}
 
-    // [START auth_fui_result]
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
         IdpResponse response = result.getIdpResponse();
         if (result.getResultCode() == RESULT_OK) {
@@ -62,19 +84,18 @@ public class SignInActivity extends AppCompatActivity {
             //Toast.makeText(getApplicationContext(), R.string.logged_in_as + user.getDisplayName(), Toast.LENGTH_LONG).show();
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
-
-        } else {
-
+        }
+        else {
             // Sign in failed
             if (response == null) {
                 // User pressed back button
                 //showSnackbar(R.string.sign_in_cancelled);
-                Toast.makeText(getApplicationContext(), R.string.sign_in_failed, Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), R.string.sign_in_failed, Toast.LENGTH_LONG).show();
                 return;
             }
 
             if (Objects.requireNonNull(response.getError()).getErrorCode() == ErrorCodes.NO_NETWORK) {
-                Toast.makeText(getApplicationContext(), R.string.no_network, Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), R.string.no_network, Toast.LENGTH_LONG).show();
                 //showSnackbar(R.string.no_internet_connection);
                 return;
             }
@@ -83,21 +104,6 @@ public class SignInActivity extends AppCompatActivity {
             //Log.e(TAG, "Sign-in error: ", response.getError());
         }
     }
-    // [END auth_fui_result]
-
-    /*public void signOut(View view) {
-        // [START auth_fui_signout]
-        AuthUI.getInstance()
-                .signOut(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(getApplicationContext(), user.getDisplayName() +" "+ R.string.logged_out, Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(view.getContext(), LoginActivity.class));
-                        finish();
-                    }
-                });
-        // [END auth_fui_signout]
-    }*/
 
     public void delete() {
         // [START auth_fui_delete]
@@ -107,80 +113,5 @@ public class SignInActivity extends AppCompatActivity {
                     // ...
                 });
         // [END auth_fui_delete]
-    }
-
-    public void themeAndLogo() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_theme_logo]
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                //.setLogo(R.drawable.my_great_logo)      // Set logo drawable
-                //.setTheme(R.style.MySuperAppTheme)      // Set theme
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_theme_logo]
-    }
-
-    public void privacyAndTerms() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_pp_tos]
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setTosAndPrivacyPolicyUrls(
-                        "https://example.com/terms.html",
-                        "https://example.com/privacy.html")
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_pp_tos]
-    }
-
-    public void emailLink() {
-        // [START auth_fui_email_link]
-        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
-                .setAndroidPackageName(
-                        /* yourPackageName= */ "com.illusion_softworks.kjoerbar",
-                        /* installIfNotAvailable= */ true,
-                        /* minimumVersion= */ null)
-                .setHandleCodeInApp(true) // This must be set to true
-                .setUrl("https://google.com") // This URL needs to be whitelisted
-                .build();
-
-        List<AuthUI.IdpConfig> providers = Collections.singletonList(
-                new AuthUI.IdpConfig.EmailBuilder()
-                        .enableEmailLinkSignIn()
-                        .setActionCodeSettings(actionCodeSettings)
-                        .build()
-        );
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_email_link]
-    }
-
-    public void catchEmailLink() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_email_link_catch]
-        if (AuthUI.canHandleIntent(getIntent())) {
-            if (getIntent().getExtras() == null) {
-                return;
-            }
-            String link = getIntent().getExtras().getString(ExtraConstants.EMAIL_LINK_SIGN_IN);
-            if (link != null) {
-                Intent signInIntent = AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setEmailLink(link)
-                        .setAvailableProviders(providers)
-                        .build();
-                signInLauncher.launch(signInIntent);
-            }
-        }
-        // [END auth_fui_email_link_catch]
     }
 }
