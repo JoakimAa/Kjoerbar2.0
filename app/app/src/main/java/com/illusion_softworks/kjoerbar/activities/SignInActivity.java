0682ab8b7c208a5ba50e6afd1dc25c0f5581679a
JoakimAa <joakimsa@hiof.no>
2021-09-14 20:05:06 +0200

    Fixed som stuff

--- app/app/src/main/java/com/illusion_softworks/kjoerbar/activities/SignInActivity.java
+++ app/app/src/main/java/com/illusion_softworks/kjoerbar/activities/SignInActivity.java
@@ -3,6 +3,7 @@ package com.illusion_softworks.kjoerbar.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,14 +39,14 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void createSignInIntent() {
-        // ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
-        //       .setAndroidPackageName(
-        //                /* yourPackageName= */ "com.illusion_softworks.kjoerbar",
-        //                /* installIfNotAvailable= */ true,
-        //                /* minimumVersion= */ null)
-        //     .setHandleCodeInApp(true) // This must be set to true
-        //    .setUrl("https://google.com") // This URL needs to be whitelisted
-        //   .build();
+        /*ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+           .setAndroidPackageName(
+                       *//* yourPackageName= *//* "com.illusion_softworks.kjoerbar",
+                       *//* installIfNotAvailable= *//* true,
+                        *//* minimumVersion= *//* null)
+           .setHandleCodeInApp(true) // This must be set to true
+           .setUrl("https://google.com") // This URL needs to be whitelisted
+           .build();*/
 
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder()
@@ -85,9 +86,11 @@ public class SignInActivity extends AppCompatActivity {
             // Successfully signed in
             user = FirebaseAuth.getInstance().getCurrentUser();
             DummyData.populateData();
-            //Toast.makeText(getApplicationContext(), R.string.logged_in_as + user.getDisplayName(), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), R.string.logged_in_as + user.getDisplayName(), Toast.LENGTH_LONG).show();
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+            Log.d("SIGNIN", "crash");
+
         }
         else {
             // Sign in failed
@@ -95,12 +98,14 @@ public class SignInActivity extends AppCompatActivity {
                 // User pressed back button
                 //showSnackbar(R.string.sign_in_cancelled);
                 //Toast.makeText(getApplicationContext(), R.string.sign_in_failed, Toast.LENGTH_LONG).show();
+                Log.d("SIGNIN", "Signin failed");
                 return;
             }
 
             if (Objects.requireNonNull(response.getError()).getErrorCode() == ErrorCodes.NO_NETWORK) {
                 //Toast.makeText(getApplicationContext(), R.string.no_network, Toast.LENGTH_LONG).show();
                 //showSnackbar(R.string.no_internet_connection);
+                Log.d("SIGNIN", "No network");
                 return;
             }
 
