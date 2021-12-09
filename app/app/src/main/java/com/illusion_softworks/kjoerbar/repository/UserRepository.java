bb07b66742cfa855576212cac994700f42d4fbbc
JoakimAa <43811556+JoakimAa@users.noreply.github.com>
2021-12-09 16:31:26 +0100

    Updated UserRepo and ViewModel
    Signed-off-by: JoakimAa <43811556+JoakimAa@users.noreply.github.com>

--- app/app/src/main/java/com/illusion_softworks/kjoerbar/repository/UserRepository.java
+++ app/app/src/main/java/com/illusion_softworks/kjoerbar/repository/UserRepository.java
@@ -2,17 +2,13 @@ package com.illusion_softworks.kjoerbar.repository;
 
 import android.util.Log;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.illusion_softworks.kjoerbar.handler.FirestoreHandler;
-import com.illusion_softworks.kjoerbar.model.Drink;
+import androidx.lifecycle.MutableLiveData;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.illusion_softworks.kjoerbar.handler.FirestoreHandler;
+import com.illusion_softworks.kjoerbar.interfaces.ICallBack;
+import com.illusion_softworks.kjoerbar.model.User;
 
 // @TODO: Unfinished
 
@@ -21,26 +17,41 @@ import java.util.List;
  * This is to prevent multiple connections to database at once
  */
 public class UserRepository {
-    private static DrinkCatalogRepository sInstance;
-    private final ArrayList<Drink> mDataSet = new ArrayList<>();
-    private final FirebaseAuth mAuthInstance = FirebaseAuth.getInstance();
-    private final FirebaseUser user = mAuthInstance.getCurrentUser();
+    private static UserRepository sInstance;
+    private final MutableLiveData<User> mDataSet = new MutableLiveData<>();
+    private static User user =  new User();
+    private final DocumentReference userDocumentReference = FirestoreHandler.getUserDocumentReference();
 
-    public static DrinkCatalogRepository getInstance() {
+    public static UserRepository getInstance() {
         if (sInstance == null) {
-            sInstance = new DrinkCatalogRepository();
+            sInstance = new UserRepository();
         }
         return sInstance;
     }
 
-    public List<Drink> getDrinks() {
+    public MutableLiveData<User> getUser(ICallBack callback) {
         // Database queries
-        getUser();
+        userDocumentReference.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                final DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Log.d("DATAHANDLER", "DocumentSnapshot data: " + document.getData());
+                    user = document.toObject(User.class);
+                    Log.d("Current user user datahandler", user.toString());
+                } else {
+                    Log.d("DATAHANDLER", "No such document");
+                }
+                mDataSet.setValue(user);
+                callback.call();
+            } else {
+                Log.d("DATAHANDLER", "get failed with ", task.getException());
+            }
+        });
         return mDataSet;
     }
 
-    private void getUser() {
-//        userDocumentReference.collection(COLLECTION_USER).get().addOnCompleteListener(task -> {
+//    private void getUser() {
+//        userDocumentReference.get().addOnCompleteListener(task -> {
 //            if (task.isSuccessful()) {
 //                for (QueryDocumentSnapshot document : task.getResult()) {
 //                    Log.d("DATAHANDLER_getUserBeverageCatalog", String.valueOf(document));
@@ -51,6 +62,6 @@ public class UserRepository {
 //                Log.w("DATAHANDLER", "Error getting user", task.getException());
 //            }
 //        });
-    }
+//    }
 
 }
---
+++ app/app/src/main/java/com/illusion_softworks/kjoerbar/viewmodel/UserViewModel.java
@@ -0,0 +1,30 @@
+package com.illusion_softworks.kjoerbar.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.illusion_softworks.kjoerbar.model.User;
+import com.illusion_softworks.kjoerbar.repository.UserRepository;
+
+public class UserViewModel extends ViewModel {
+    private MutableLiveData<User> mUser;
+    private final MutableLiveData<Boolean> mIsUpdating = new MutableLiveData<>();
+
+    public void init() {
+       /* if (mSessions != null) {
+            return;
+        }*/
+        mIsUpdating.setValue(true);
+        UserRepository mRepository = UserRepository.getInstance();
+        mUser = mRepository.getUser(() -> mIsUpdating.setValue(false));
+    }
+
+    public LiveData<User> getUser() {
+        return mUser;
+    }
+
+    public LiveData<Boolean> getIsUpdating() {
+        return mIsUpdating;
+    }
+}
