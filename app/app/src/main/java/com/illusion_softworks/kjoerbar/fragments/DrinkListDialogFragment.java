68226e27f0ae4803ddafb5c91050df8f7d6ebfbb
42-Broenstudentavis <Lars.e.faber@gmail.com>
2021-12-09 17:48:40 +0100

    Added recyclerview to bottom sheet dialog

--- app/app/src/main/java/com/illusion_softworks/kjoerbar/fragments/DrinkListDialogFragment.java
+++ app/app/src/main/java/com/illusion_softworks/kjoerbar/fragments/DrinkListDialogFragment.java
@@ -2,23 +2,51 @@ package com.illusion_softworks.kjoerbar.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.illusion_softworks.kjoerbar.R;
+import com.illusion_softworks.kjoerbar.adapter.DrinkRecyclerAdapter;
+import com.illusion_softworks.kjoerbar.handler.UserDataHandler;
+import com.illusion_softworks.kjoerbar.interfaces.OnItemClickListener;
+import com.illusion_softworks.kjoerbar.model.Drink;
 import com.illusion_softworks.kjoerbar.viewmodel.DrinkCatalogViewModel;
 
-public class DrinkListDialogFragment extends BottomSheetDialogFragment {
+import java.util.List;
+
+public class DrinkListDialogFragment extends BottomSheetDialogFragment implements OnItemClickListener {
     public static final String TAG = "BottomSheetAddDrinkFragment";
+    private static final List<Drink> data = UserDataHandler.getDrinks();
+    private DrinkRecyclerAdapter mAdapter;
+    private ProgressBar mProgressBar;
+
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
 
     public static DrinkListDialogFragment newInstance(Context context) {
         return new DrinkListDialogFragment();
+    }
+
+    private void initRecyclerView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.beverageRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -41,7 +69,38 @@ public class DrinkListDialogFragment extends BottomSheetDialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        mProgressBar = view.findViewById(R.id.progress_bar);
+
         DrinkCatalogViewModel mViewModel = new ViewModelProvider(requireActivity()).get(DrinkCatalogViewModel.class);
         mViewModel.init();
+
+        mAdapter = new DrinkRecyclerAdapter(view.getContext(), this);
+        mViewModel.getDrinks().observe(getViewLifecycleOwner(), drinks -> mAdapter.addDataSet(drinks));
+        mViewModel.getIsUpdating().observe(getViewLifecycleOwner(), isLoading -> {
+            if (isLoading) {
+                showProgressBar();
+            } else {
+                hideProgressBar();
+            }
+        });
+
+        initRecyclerView(view);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        SessionFragment.startNewSession();
+        SessionFragment.addDrink(data.get(position));
+        Navigation.findNavController(requireActivity(), R.id.nav_host).navigate(R.id.sessionFragment);
+        Log.d("TAG", "onItemClick: " + data.get(position));
+        // Close bottom dialog
+        this.dismiss();
+    }
+
+    @Override
+    public void onItemClick(String view) {
+        if (view.equals("beverageDetailFragment")) {
+            Navigation.findNavController(requireActivity(), R.id.nav_host).navigate(R.id.action_addDrinkFragment_to_drinkDetailFragment);
+        }
     }
 }
